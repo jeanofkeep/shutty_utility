3bc3fff393dac4de90c7e3dc3cced659a06f76f0
jeanofkeep <vadimmoshkov56@gmail.com>
2025-10-31 17:09:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace utility
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+ 
+
+            var process = new Process();
+
+            process.StartInfo.FileName = "file_name.exe";
+            process.Start();
+
+        }
+    }
+}
